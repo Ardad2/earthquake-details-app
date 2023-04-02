327d4f50821951318855934e4f80c174e4eb875f
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-02 09:10:38 -0700

    add everything

---
+++ CSE335Lab7/CSE335Lab7/CSE335Lab7App.swift
@@ -0,0 +1,19 @@
+//
+//  CSE335Lab7App.swift
+//  CSE335Lab7
+//
+//  Created by Arjun Dadhwal on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CSE335Lab7App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSE335Lab7/CSE335Lab7/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  CSE335Lab7
+//
+//  Created by Arjun Dadhwal on 4/2/23.
+//
+
+import SwiftUI
+//import CoreData
+
+struct ContentView: View {
+    //@Environment(\.managedObjectContext) private var viewContext
+    
+    //@FetchRequest(
+    //private var items: FetchedResults<Item>
+    
+    var body: some View {
+        NavigationView {
+            Text("test")
+        }
+    }
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()    }
+    }
+}
