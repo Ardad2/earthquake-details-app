d426065d822d9194a7673506f5841d4b922b725d
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-02 09:46:55 -0700

    adding location variables

--- CSE335Lab7/CSE335Lab7/ContentView.swift
+++ CSE335Lab7/CSE335Lab7/ContentView.swift
@@ -5,14 +5,17 @@
 //  Created by Arjun Dadhwal on 4/2/23.
 //
 
+import CoreLocation
+import MapKit
 import SwiftUI
 //import CoreData
 
 struct ContentView: View {
-    //@Environment(\.managedObjectContext) private var viewContext
     
-    //@FetchRequest(
-    //private var items: FetchedResults<Item>
+    private static let defaultLocation = CLLocationCoordinate2D(
+        latitude: 33.4255,
+        longitude: -111.9400
+    )
     
     var body: some View {
         NavigationView {
@@ -20,11 +23,31 @@ struct ContentView: View {
         }
     }
     
+    /*
+    
+    func getLocation(from address: String, completion: @escaping (_ location:CLLocationCoordinate2D?) -> Void) {
+        let geocoder = CLGeocoder();
+        
+        geocoder.geocodeAddressString(address) { (placemarks, error) in guard let placemarks = placemarks,
+            
+            let location = placemarks.first?.location?.coordinate
+            else {
+                completion(nil);
+                return
+            }
+
+                    region.center = location
+            markers[0] = Location(name: cityName, coordinate: location);
+
+
+        }
+    } */
     
     
-    
+    /*
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()    }
     }
+    */
 }
